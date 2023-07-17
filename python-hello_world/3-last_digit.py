697941b227f984239ef52bd23aabe4aab9dd2bd6
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-07-17 08:23:14 -0700

    getting last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,15 +1,11 @@
-
 import random
-
 number = random.randint(-10000, 10000)
 """
 To get the last digit of a number, use
 the modulos operator
 """
 last_digit = abs(number) % 10
-
 print("Last digit of", number, "is", last_digit, end=" ")
-
 if (last_digit > 5):
     print("and is greater than 5")
 elif (last_digit == 0):
