e37110a32c0adcbc87dcd8b56ba13ef85169b702
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-07-19 08:50:27 -0700

    reversed string

---
+++ python-functions/3-string.py
@@ -0,0 +1,10 @@
+def reverse_string(string):
+    reversed_string = ""
+    for i in range(len(string) - 1, -1, -1):
+        reversed_string += string[i]
+    return reversed_string
+"""
+input_string = "Danso Emmanuel"
+reverse_string = reverse_string(input_string)
+print(reverse_string)
+"""
