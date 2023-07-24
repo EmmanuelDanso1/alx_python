18eb0e899adf4f7898c731dbd3a51b73fc6066ac
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-07-24 11:42:03 -0700

    update

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,19 +1,11 @@
 # add_0.py
 def add(a, b):
     return a + b
-def import_add_function(file_name):
-    with open(file_name) as f:
-        code = compile(f.read(), file_name, 'exec')
-        namespace = {}
-        exec(code, namespace)
-        return namespace.get('add', None)
-
-
 if __name__ == "__main__":
     a = 1
     b = 2
 
-    add_function = import_add_function('add_0.py')
-    if add_function is not None:
-        result = add_function(a, b)
-        print("{} + {} = {}".format(a, b, result))
+    from add_0 import add
+
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
