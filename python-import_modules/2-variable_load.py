a2d32c53d0723891a37b1a1ff514f84c08ee7981
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-07-24 09:45:37 -0700

    a program that imports the variable a from the file variable_load_2.py and prints its value

---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#Assigning a variable 2-variable_load.py and printing the value out
+#2-variable_load.py
+a = 7
+
+def import_variable_load(file):
+    with open(file) as v:
+        new_code = compile(v.read(), file, 'exec')
+        namespace = {}
+        exec(new_code,namespace)
+        return namespace.get('a', None)
+"""
+In order to make it not run, we use if statement if __name__ == __main__
+"""
+if __name__ == "__main__":
+    imported_a = import_variable_load('2-variable_load.py')
+    if imported_a is not None:
+        print("The value of 'a is:", imported_a)
+    else:
+        print("The variable 'a' was not found in the file.")
