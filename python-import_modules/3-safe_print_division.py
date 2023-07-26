194bb478392d8c57ec1373b051dbb76aea15f38a
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-07-26 06:48:35 -0700

    modified

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,8 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivision
-    Error:
+    except:
         result = None
     finally:
         print("Inside result: {}".format(result))
