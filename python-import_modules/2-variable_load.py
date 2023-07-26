79a8e8e2abf73150642b5a7c2a10f1d20efe1860
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-07-26 06:38:22 -0700

    An import

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -3,22 +3,7 @@
 Assigning a variable 2-variable_load.py and printing the value out
 #2-variable_load.py
 """
-a = 89
-a = -100
-
-
-def import_variable_load(variable_load_2):
-    with open(variable_load_2) as f:
-        new_code = compile(f.read(), variable_load_2, 'exec')
-        namespace = {}
-        exec(new_code, namespace)
-        return namespace.get('a', None)
-"""
-In order to make it not run, we use if statement if __name__ == __main__
-"""
 if __name__ == "__main__":
-    imported_a = import_variable_load('2-variable_load.py')
-    if imported_a is not None:
-        print("The value of 'a is:", imported_a)
-    else:
-        print("The variable 'a' was not found in the file.")
+    from variable_load_2 import a
+
+    print(a)
