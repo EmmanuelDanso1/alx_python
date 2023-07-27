e943ee233a37e84cd1b64aaf94676d9ca68dc457
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-07-27 06:50:38 -0700

    updated

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 #returning common set
 def common_elements(set_1, set_2):
-    common_set_returning = set_1 & set_2
-    return common_set_returning
+    #common_set_returning = set_1 & set_2
+    #return common_set_returning
+    common_set_callback = set()
+
+    for element in set_1:
+        if element in set_2:
+            common_set_callback.add(element)
+    return common_set_callback
