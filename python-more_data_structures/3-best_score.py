9793818f468c200fc05436dd3e3883c6208781c7
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-07-27 07:16:12 -0700

    updated

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_value = None
-    max_key = None
+    maximum_value = None
+    maximum_key = None
 
 
     for key, value in a_dictionary.items():
-        if max_value is None or value > max_value:
-            max_value = value
-            max_key = key
-    return max_key
+        if maximum_value is None or value > maximum_value:
+            maximum_value = value
+            maximum_key = key
+    return maximum_key
