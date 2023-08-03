7b892df7667656095553ce2709d8c1927ea8016d
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-03 03:57:55 -0700

    exceptions

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module defines class BaseGeometry, which chekcs for geometry
+    BaseGeometry:
+            This checks for geometry-related classes
+"""
+class BaseGeometry:
+    """
+    A base class that provides a placeholder method for calculating the area of a geometric shape.
+
+    This class is designed to be subclassed and extended with specific methods and attributes
+    to create more specialized geometric classes.
+
+    Public Instance Method:
+        area(self): Raises an Exception with the message "area() is not implemented".
+    """
+    def area(self):
+        """
+        Raises an Exception indicating that the area() method is not implemented in the current class.
+
+        This method serves as a placeholder to be overridden in subclasses that provide the actual
+        implementation of calculating the area for specific geometric shapes.
+
+        Raises:
+            Exception: Always raises an Exception with the message "area() is not implemented".
+        """
+    raise Exception("area() is not implemented")
