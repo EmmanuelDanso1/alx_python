dbb3ed32d0790ab2d24ea89b43cfe8128bc8caad
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-03 02:45:36 -0700

    inherit from object

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a inherits_from, which checks for boolean
+inherits_from:
+            This checks true for instance of a specific object otherwise, false
+"""
+def inherits_from(obj, a_class):
+    """
+    Check if the object is exactly an instance of the specified class.
+
+    Parameters:
+        obj: Any-The object to be checked.
+        a_class: type - The class to be compared with.
+
+    Returns:
+        boolean: True if the object is an instance of the specified class,else, False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
