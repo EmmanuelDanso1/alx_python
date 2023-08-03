f3d23f814a1413d0d3e137f0131409feb6f2356f
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-03 03:30:00 -0700

    modified

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,6 +6,9 @@ This module defines class BaseGeometry, which chekcs for geometry
 """
 
 
+from collections.abc import Iterable
+
+
 class BaseGeometry:
     """
     A base class representing the foundation for geometry-related classes.
@@ -13,4 +16,10 @@ class BaseGeometry:
     This class serves as a base for other classes that deal with geometry.
     It is an empty class, meaning it does not have methods or attributes of its own
     """
-    pass
+    def __dir__(cls) -> None:
+        """
+        controls access to inherited attributes
+        """
+        attributes = super().__dir__()
+
+        return[attribute for attribute in attributes if attribute != '__init_subclass__']
