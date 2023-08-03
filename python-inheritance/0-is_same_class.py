3e180e7145849ab2314f4df01f07bd77fea51e11
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-03 02:08:40 -0700

    modified boolean

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,4 +2,14 @@
 #function that returns True 
 #if the object is exactly an instance of the specified class ; otherwise False
 def is_same_class(obj, a_class):
+    """
+    Check if the object is exactly an instance of the specified class.
+
+    Parameters:
+        obj: Any-The object to be checked.
+        a_class: type - The class to be compared with.
+
+    Returns:
+        boolean: True if the object is an instance of the specified class,else, False.
+    """
     return type(obj) is a_class #using 'is' to check for the boolean
