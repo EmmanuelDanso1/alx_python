8a263f3dc916541bb64dfb5652bcfdd1610636cd
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-03 03:31:31 -0700

    modified

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -16,10 +16,12 @@ class BaseGeometry:
     This class serves as a base for other classes that deal with geometry.
     It is an empty class, meaning it does not have methods or attributes of its own
     """
+    pass
+class newClass():
     def __dir__(cls) -> None:
-        """
-        controls access to inherited attributes
-        """
+
         attributes = super().__dir__()
 
         return[attribute for attribute in attributes if attribute != '__init_subclass__']
+        
+
