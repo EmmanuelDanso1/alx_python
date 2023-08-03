a04676c0f360079871714c1c295e6098c986502c
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-03 03:05:19 -0700

    geometry related classes

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines class BaseGeometry, which chekcs for geometry
+    BaseGeometry:
+            This checks for geometry-related classes
+"""
+class BaseGeometry:
+    """
+    A base class representing the foundation for geometry-related classes.
+
+    This class serves as a base for other classes that deal with geometry.
+    It is an empty class, meaning it neither have methods nor attributes of its own
+    """
