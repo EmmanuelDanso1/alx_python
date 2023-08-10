5e276df7522833655465f55d4594839bc3c65e59
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-10 03:56:06 -0700

    new update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """
 Using the import  to import the requests package.
+The imported package is used to access data from a URL or any link
 """
 import requests
+
 #url to fetch the data from the intranet
 url = "https://alu-intranet.hbtn.io/status"
 
+#sends GET request to the url
+response = requests.get(url)
 
-#using Get method to send a get request to server
+#checks if the response status code is 200
 if response.status_code ==  200:
         print("Body response:")
-        print("\t - type:", type(data))
-        print("\t - content:",data)
+        print("\t- type:", type(response.text))
+        print("\t- content:", response.text)
