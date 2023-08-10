4dbb2acea9e53b01d4be7ef0a76bdc039f694ea3
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-10 01:43:59 -0700

    updated

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,20 +4,20 @@ This module sends request to the server and gets a feedback
 By using thr import module to import the requests package.
 """
 import requests
-"""
-url to fetch the data from the intranet
-"""
+#url to fetch the data from the intranet
+
 url = "https://alu-intranet.hbtn.io/status"
 
-"""
-using get method to fetch response  from server
-"""
+
+#using get method to fetch response  from server
 response = requests.get(url)
 
-if response.status_code ==  200:
-    """
+"""
     using if statement to check if data is in json format
     and checking if the status code is ok
-    """
+"""
 if response.status_code ==  200:
     data = response.json()
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:",data)
