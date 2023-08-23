93d907f04233c223ede631e300fc148366686f14
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-23 08:36:51 -0700

    display cities in ascending order

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+a script displays all cities
+values in the states table of hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+def list_cities(username, password, db_name, state_name):
+    # Establishing a connection to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+    """
+    Creating a cursor object to
+    interact with the database
+
+    """
+    cursor = db.cursor()
+    """
+    Creating and executing
+    the SQL query using user input
+    """
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    cursor.execute(query)
+    # Fetch all matching rows
+    rows = cursor.fetchall()
+    # Display the results
+    for row in rows:
+        print(row)
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <mysql_username> <mysql_password> <db_name>")
+    else:
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        db_name = sys.argv[3]
+        list_cities(mysql_username, mysql_password, db_name)
