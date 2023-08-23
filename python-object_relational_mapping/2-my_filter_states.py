92030c63c2f325e74e0925a779e54a6ce06b9ded
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-08-23 08:15:29 -0700

    updated

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -25,22 +25,23 @@ def display_matching_states(username, password, db_name, state_name):
     Creating and executing
     the SQL query using user input
     """
-    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-    # Fetching all matching rows
+    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(state_name)
+    cursor.execute(query)
+    # Fetch all matching rows
     rows = cursor.fetchall()
     # Display the results
     for row in rows:
         print(row)
-    # Closing the cursor and connection
+    # Close the cursor and connection
     cursor.close()
     db.close()
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: python script.py mysql_user mysql_password db_name state_name")
+        print("Usage: python script.py <mysql_username> <mysql_password> <db_name> <state_name>")
     else:
         mysql_username = sys.argv[1]
         mysql_password = sys.argv[2]
         db_name = sys.argv[3]
         state_name = sys.argv[4]
         display_matching_states(mysql_username, mysql_password, db_name, state_name)
+        
